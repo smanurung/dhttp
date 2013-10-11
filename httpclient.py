06a3b07a4ae7f9ee3eeaada22e0e91856413bbb2
Sonny Theo Tumbur Manurung <sonthehottest@gmail.com>
2013-10-11 13:37:00 +0700

    Delete Psw

--- httpclient.py
+++ httpclient.py
@@ -1,4 +1,4 @@
 import urllib
 
-login_data = urllib.urlencode({'email':'sonnyportnoy@yahoo.com','pass':'sonnytheot','login':'Masuk'})
+login_data = urllib.urlencode({'email':'sonnyportnoy@yahoo.com','pass':'','login':'Masuk'})
 op = urllib.urlopen('https://www.facebook.com/login.php?next=http%3A%2F%2Fwww.facebook.com%2Fhome.php',login_data)
